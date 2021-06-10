e0827230190353c50f1329233e89c2707e190e9c
Michal Nowak <michal.nowakb@gmail.com>
2021-06-10 13:40:27 +0100

    new commands added for vagrant, script to run app added

--- Vagrantfile
+++ Vagrantfile
@@ -2,6 +2,9 @@ Vagrant.configure("2") do |config|
 
   config.vm.box = "ubuntu/xenial64"
   config.vm.network "private_network", ip: "192.168.10.100"
+  config.vm.synced_folder "app", "/home/vagrant/app"
+  config.vm.provision "shell", path: "app/provision.sh"
+
 end
 
 
---
+++ app/provision.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt-get update -y
+
+sudo apt-get upgrade -y
+
+sudo apt-get install nginx -y
+
+sudo apt-get install nodejs -y
+
+sudo apt-get install python-software-properties -y
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+
+sudo apt-get install nodejs -y
+
+sudo npm install pm2 -g
+
+cd app
+
+sudo npm install
+
+node app.js
